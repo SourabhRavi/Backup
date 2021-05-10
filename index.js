0885ac640ea5b1449f53a9fa88ebf1d542c2aee1
Sourabh Kumar Ravi <66831910+SourabhRavi@users.noreply.github.com>
2021-05-10 12:55:11 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Object Array
+let noteList = [{}];
+
+
+let addNewNote = (content, taskDeadline, dateCreated, dateModified, category) => {
+    this.content = content;
+    this.taskDeadline = taskDeadline;
+    this.dateCreated = dateCreated;
+    this.dateModified = dateModified;
+    this.category = category;
+};
+
+let addNote = document.getElementById("addNote");
